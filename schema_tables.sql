6d3ba1a79df1d3f1b249cfe95b5bad5608ad10ae
Abel Tavares <121238257+abeltavares@users.noreply.github.com>
2023-02-02 01:10:31 +0000

    SQL

---
+++ schema_tables.sql
@@ -0,0 +1,26 @@
+-- Create a schema for the project
+CREATE SCHEMA dashboard;
+
+-- Create a table to store the stock data
+CREATE TABLE IF NOT EXISTS dashboard.stock_data (
+  id serial PRIMARY KEY,
+  symbol varchar(50) NOT NULL,
+  date timestamp NOT NULL,
+  open numeric(10, 2) NOT NULL,
+  high numeric(10, 2) NOT NULL,
+  low numeric(10, 2) NOT NULL,
+  close numeric(10, 2) NOT NULL,
+  volume numeric(10, 2) NOT NULL
+);
+
+-- Create a table to store the crypto data
+CREATE TABLE IF NOT EXISTS crypto_data (
+  id serial PRIMARY KEY,
+  symbol varchar(50) NOT NULL,
+  date timestamp NOT NULL,
+  open numeric(10, 2) NOT NULL,
+  high numeric(10, 2) NOT NULL,
+  low numeric(10, 2) NOT NULL,
+  close numeric(10, 2) NOT NULL,
+  volume numeric(10, 2) NOT NULL
+);  
