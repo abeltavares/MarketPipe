292dbd8df1d3a7ed343f5790bf3d6fd23a02929c
Abel Tavares <121238257+abeltavares@users.noreply.github.com>
2023-02-02 14:32:32 +0000

    commit

--- data_collection_storage.py
+++ data_collection_storage.py
@@ -7,13 +7,23 @@ import psycopg2
 # Function for retrieving top gainers
 def get_top_gainers():
     """Get the top gainers from the financialmodelingprep API"""
-    url = "https://financialmodelingprep.com/api/v3/stock_market \
-        /gainers?apikey=e18f8efccbac4ac741c48162fec73d2e"
+    url = "https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=e18f8efccbac4ac741c48162fec73d2e"
     response = requests.get(url, timeout=5)
     if response.status_code != 200:
         raise Exception(f"Failed to retrieve data from the API: {response.text}")
     data = response.json()
     return data
+
+# Function for retrieving company name
+def get_company_name(symbol):
+    """Get the name from the company profile information"""
+    url = f"https://financialmodelingprep.com/api/v3/profile/{symbol}?apikey=e18f8efccbac4ac741c48162fec73d2e"
+    response = requests.get(url, timeout=5)
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve data for {symbol} from the API: {response.text}")
+    company_data = response.json()[0]['companyName']
+    # Return the company name
+    return company_data
 
 # List of the 5 top gainers
 top_gainers_data = get_top_gainers()
@@ -27,8 +37,7 @@ def get_stock_data(symbols):
     """Get the stock data for a list of symbols from the Alpha Vantage API"""
     stock_info = {}
     for symbol in symbols:
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol= \
-            {symbol}&interval=5min&apikey=S1HB81M1BIAB0ML2'
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey=S1HB81M1BIAB0ML2'
         response = requests.get(url, timeout=5)
         if response.status_code != 200:
             raise Exception(f"Failed to retrieve data for {symbol} from the API: {response.text}")
@@ -54,6 +63,9 @@ def store_data_in_postgresql(data):
 
         # Loop through the stock data
         for symbol, stock_info in data.items():
+            # Get the company name for the symbol
+            name = get_company_name(symbol)
+
             # Extract the relevant information
             date = datetime.datetime.strptime(stock_info["Meta Data"]["3. Last Refreshed"], \
                  '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
@@ -63,9 +75,9 @@ def store_data_in_postgresql(data):
             close_price = stock_info["Time Series (5min)"][date]["4. close"]
             volume = stock_info["Time Series (5min)"][date]["5. volume"]
             # Insert the data into the table
-            cur.execute("INSERT INTO dashboard_data.stock_data \
-                (symbol, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                        (symbol, date, open_price, high, low, close_price, volume))
+            cur.execute("INSERT INTO dashboard.stock_data \
+                (symbol, name, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                        (symbol, name, date, open_price, high, low, close_price, volume))
         conn.commit()
     except psycopg2.Error as error:
         print(f"An error occurred while storing the data in the database: {error}")
--- schema_tables.sql
+++ schema_tables.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA dashboard_data;
 -- Create a table to store the stock data
 CREATE TABLE IF NOT EXISTS dashboard_data.stock_data (
   id serial PRIMARY KEY,
-  symbol varchar(50) NOT NULL,
+  stock varchar(50) NOT NULL,
   name varchar(255) NOT NULL,
   date timestamp NOT NULL,
   open numeric(10, 2) NOT NULL,
