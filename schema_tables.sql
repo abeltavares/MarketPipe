462b9bf1b37e74669523d12d5d7f51a694acf98f
Abel Tavares <121238257+abeltavares@users.noreply.github.com>
2023-02-02 17:06:28 +0000

    Update schema_tables.sql

--- schema_tables.sql
+++ schema_tables.sql
@@ -13,16 +13,3 @@ CREATE TABLE IF NOT EXISTS dashboard_data.stock_data (
   close numeric(10, 2) NOT NULL,
   volume numeric(10, 2) NOT NULL
 );
-
-
--- Create a table to store the crypto data
-CREATE TABLE IF NOT EXISTS dashboard_data.crypto_data (
-  id serial PRIMARY KEY,
-  symbol varchar(50) NOT NULL,
-  date timestamp NOT NULL,
-  open numeric(10, 2) NOT NULL,
-  high numeric(10, 2) NOT NULL,
-  low numeric(10, 2) NOT NULL,
-  close numeric(10, 2) NOT NULL,
-  volume numeric(10, 2) NOT NULL
-);  
