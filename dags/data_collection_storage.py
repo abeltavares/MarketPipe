89b2211b8a962fcf10697986ff84c267893ce157
Abel Tavares <121238257+abeltavares@users.noreply.github.com>
2023-03-19 01:25:30 +0000

    add backup function

--- dags/data_collection_storage.py
+++ dags/data_collection_storage.py
@@ -1,28 +1,42 @@
 """
 A Python script with functions for retrieving stock and crypto performance data from Alpha Vantage, Financial Modeling Prep and CoinMarketCap APIs and storing the data in a PostgreSQL database.
+
+Additionally, the script includes a backup_data function for extracting and storing the data from the database to an S3 bucket on a monthly basis.
 """
 
 # Import necessary modules
 import requests
-import datetime
 import time
 import os
 import psycopg2
+import boto3
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import psycopg2
+from datetime import datetime, timedelta
 
 
 # Define the base URL and API key for Alpha Advantage API
 ALPHA_BASE_URL = "https://www.alphavantage.co/query?"
-ALPHA_API_KEY = "S1HB81M1BIAB0ML2"  #replace with your API key
+ALPHA_API_KEY = os.environ['ALPHA_API_KEY']
 
 # Define the base URL and API key for Financial Modeling Prep API
 PREP_BASE_URL = "https://financialmodelingprep.com/api/v3/"
-PREP_API_KEY = "601e5192acccbaeee4ed7e15f16c1200"  #replace with your API key
+PREP_API_KEY = os.environ['PREP_API_KEY']
 
 # Define the API key for CoinMarketCap API
-COIN_API_KEY = '08012b12-c8f6-482f-821d-0fa6dbce76e3'
+COIN_API_KEY = os.environ['COIN_API_KEY']
 
 # These variables are used to make API requests to Alpha Advantage, Financial Modeling Prep and CoinMarketCap 
 # The base URL and API key are used to build the complete URL to make the request
+
+# Get the database configuration from environment variables
+host = os.environ.get("POSTGRES_HOST")
+port = os.environ.get("POSTGRES_PORT")
+database = os.environ.get("POSTGRES_DB")
+user = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
 
 
 def get_stocks() -> dict:
@@ -123,7 +137,7 @@ def get_stock_data(symbols: dict) -> dict:
     return stock_data
 
 
-def store_stock_data(data: dict):
+def store_stock_data(data: dict)-> None:
     """
     Store the stock market data in a PostgreSQL database
     :param data: A dictionary with keys 'gainers', 'losers', and 'actives', each with a list of stock data
@@ -131,13 +145,6 @@ def store_stock_data(data: dict):
 
     # Set the schema name to use for storing the stock data
     schema_name = "stock_data"
-
-    # Get the database configuration from environment variables
-    host = os.environ.get("POSTGRES_HOST")
-    port = os.environ.get("POSTGRES_PORT")
-    database = os.environ.get("POSTGRES_DB")
-    user = os.environ.get("POSTGRES_USER")
-    password = os.environ.get("POSTGRES_PASSWORD")
 
     # Connect to the database
     conn = None
@@ -277,7 +284,7 @@ def get_crypto_data() -> dict:
         return None
 
 
-def store_crypto_data(data: dict):
+def store_crypto_data(data: dict) -> None:
     """
     Store the crypto market data in a PostgreSQL database
     :param data: A dictionary with keys 'gainers', 'losers', and 'actives', each with a list of crypto data
@@ -285,13 +292,6 @@ def store_crypto_data(data: dict):
 
     # Set the schema name to use for storing the crypto data
     schema_name = "crypto_data"
-
-    # Get the database configuration from environment variables
-    host = os.environ.get("POSTGRES_HOST")
-    port = os.environ.get("POSTGRES_PORT")
-    database = os.environ.get("POSTGRES_DB")
-    user = os.environ.get("POSTGRES_USER")
-    password = os.environ.get("POSTGRES_PASSWORD")
 
     # Connect to the database
     conn = None
@@ -345,3 +345,77 @@ def store_crypto_data(data: dict):
         if conn:
             conn.close()
  
+
+def backup_data(bucket_name: str) -> None:
+    """
+    Extracts and stores data from database to S3 for the current month.
+    
+    Args:
+    - bucket_name (str): The name of the S3 bucket to store the data in.
+    """
+    # Connect to the database
+    conn = None
+    cur = None
+    try:
+        # Connect to the database using the configuration from environment variables
+        conn = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
+
+        # Get the current month
+        current_month = datetime.today().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        start_date = current_month.strftime('%Y-%m-%d')
+        end_date = (current_month + timedelta(days=31)).strftime('%Y-%m-%d')
+        
+        # Define the SQL query to extract the data for the current month
+        # Define a list of performance types and their corresponding tables
+        performance_types = {
+            'stocks_gainers': 'stock_data.gainers',
+            'stocks_losers': 'stock_data.losers',
+            'stocks_actives': 'stock_data.actives',
+            'crypto_gainers': 'crypto_data.gainers',
+            'crypto_losers': 'crypto_data.losers',
+            'crypto_actives': 'crypto_data.actives'
+        }
+
+        # Initialize an empty list to store the generated SQL queries
+        queries = []
+
+        # Loop through each performance type and table, and generate the corresponding SQL query
+        for performance_type, table in performance_types.items():
+            query = (f"SELECT '{performance_type}' as performance_type, "
+                    f"date_collected, symbol, name, market_cap, volume, "
+                    f"price, change_percent FROM market_data.{table} "
+                    f"WHERE date_collected >= '{start_date}' AND date_collected < '{end_date}'")
+
+            queries.append(query)
+
+        # Combine all queries using UNION ALL to create the final SQL statement
+        final_query = " UNION ALL ".join(queries)
+
+
+        # Execute the query and retrieve the data
+        cur = conn.cursor()
+        cur.execute(final_query)
+        rows = cur.fetchall()
+
+        # Create a Pandas DataFrame with the retrieved data
+        df = pd.DataFrame(rows, columns=[desc[0] for desc in cur.description])
+
+        # Store the data as a Parquet file in an S3 bucket
+        s3 = boto3.resource('s3')
+        key = f"market_data/{current_month.year}/{current_month.month}/data.parquet"
+        pq.write_table(pa.Table.from_pandas(df), f"s3://{bucket_name}/{key}")
+    
+    except Exception as e:
+        print(f"Error backing up data to S3: {e}")
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+            
